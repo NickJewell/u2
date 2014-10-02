15393a449e1041c75c0b4d555f922c7be0f65f9c
Nick Jewell <nick.jewell@gmail.com>
2014-10-02 21:40:11 +0100

    Implemented User Login and Sign up

--- u2/urls.py
+++ u2/urls.py
@@ -5,8 +5,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'u2.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'u2_app.views.index'), # root
+    url(r'^login$', 'u2_app.views.login_view'), # login
+    url(r'^logout$', 'u2_app.views.logout_view'), # logout
+    url(r'^signup$', 'u2_app.views.signup'), # signup
 )
--- u2_app/views.py
+++ u2_app/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from u2_app.forms import AuthenticateForm, UserCreateForm, U2Form
+from u2_app.models import U2
 
-# Create your views here.
+def index(request, auth_form=None, user_form=None):
+    # User is logged in
+    if request.user.is_authenticated():
+        u2_form = U2Form()
+        user = request.user
+        u2_self = U2.objects.filter(user=user.id)
+        u2_buddies = U2.objects.filter(user__userprofile__in=user.profile.follows.all)
+        u2 = u2_self | u2_buddies
+ 
+        return render(request,
+                      'buddies.html',
+                      {'u2_form': u2_form, 'user': user,
+                       'u2': u2,
+                       'next_url': '/', })
+    else:
+        # User is not logged in
+        auth_form = auth_form or AuthenticateForm()
+        user_form = user_form or UserCreateForm()
+ 
+        return render(request,
+                      'home.html',
+                      {'auth_form': auth_form, 'user_form': user_form, })
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticateForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            # Success
+            return redirect('/')
+        else:
+            # Failure
+            return index(request, auth_form=form)
+    return redirect('/')
+ 
+ 
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+	
+def signup(request):
+    user_form = UserCreateForm(data=request.POST)
+    if request.method == 'POST':
+        if user_form.is_valid():
+            username = user_form.clean_username()
+            password = user_form.clean_password2()
+            user_form.save()
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+        else:
+            return index(request, user_form=user_form)
+    return redirect('/')
